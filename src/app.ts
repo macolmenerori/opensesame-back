ae1b17cb109822c19331184d7d4a0837cdc5039c
macolmenerori <miancolrin@hotmail.com>
2024-07-08 17:55:23 +0200

    Added security headers

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
+import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import express from 'express';
+import mongoSanitize from 'express-mongo-sanitize';
+import { rateLimit } from 'express-rate-limit';
+import helmet from 'helmet';
 
 import userRouter from './routes/userRouter';
 
 const app = express();
+
+// Add security HTTP headers
+app.use(helmet({ contentSecurityPolicy: false }));
+
+// Rate limit. Default is 100 requests per hour
+const limiter = rateLimit({
+  max: parseInt(
+    process.env.RATELIMIT_MAXCONNECTIONS ? process.env.RATELIMIT_MAXCONNECTIONS : '100'
+  ),
+  windowMs: parseInt(process.env.RATELIMIT_WINDOWMS ? process.env.RATELIMIT_WINDOWMS : '3600000'),
+  message: 'Too many requests from this IP, please try again after an hour'
+});
+app.use('/api', limiter);
 
 // Middleware, modifies incoming data. For parsing JSON bodies on POST requests
 app.use(express.json({ limit: '10kb' })); // Do not accept bodies bigger than 10 kilobytes
@@ -13,11 +30,25 @@ app.use(express.urlencoded({ extended: true, limit: '10kb' })); // Do not accept
 
 app.use(cookieParser()); // Parse cookies
 
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Compress responses
+app.use(compression());
+
 // TODO: Delete this
 app.get('/', (req, res) => {
   res.send('Hello from opensesame!');
 });
 
 app.use('/api/v1/users', userRouter);
+
+// Middleware for handling unhandled routes
+app.all('*', (req, res) => {
+  return res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+});
 
 export default app;
