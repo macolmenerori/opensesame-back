f50741b8088ce55a4c8a312898d25dfb7364192d
macolmenerori <miguelangelcolmenero@gmail.com>
2024-07-30 16:27:51 +0200

    Removed unused variables

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,10 +1,10 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */ // TODO: Fix this type
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextFunction, Request, Response } from 'express';
 
 const catchAsync =
   (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) =>
   (req: Request, res: Response, next: NextFunction) => {
-    fn(req, res, next).catch((e) => {
+    fn(req, res, next).catch(() => {
       return res.status(500).json({
         status: 'fail',
         message: 'Internal server error.'
